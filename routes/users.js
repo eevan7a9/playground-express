f11a365a6381762f997c0155240041b0d351ae73
eevan7a9 <e7a9@protonmail.com>
2021-07-13 10:42:00 +0900

    perf(users): post new user

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,35 @@
-import express from 'express';
-import { users } from '../dummy-data/users.js';
+import express from "express";
+import { users } from "../dummy-data/users.js";
 
 const router = express.Router();
 
 // users root
-router.get('/', (req, res) => {
-    console.log('Getting all users...');
-    res.send(users);
+router.get("/", (_, res) => {
+  console.log("Getting all users...");
+  res.send(users);
+});
+// New user
+router.post("/", (req, res) => {
+  console.log("Adding new user");
+  users.push(req.body);
+  res.send({
+    ...{
+      message: "Success: New user added.",
+    },
+    data: req.body,
+  });
 });
 // single user by id
-router.get('/:id', (req, res) => {
-    console.log('Getting single user...');
-    const user = users.find((user) => user.id === parseInt(req.params.id));
-    if (user) {
-        res.send(user);
-        return;
-    }
-    res.send({ message: 'Error: User not found.' });
-    
+router.get("/:id", (req, res) => {
+  console.log("Getting single user...");
+  const user = users.find((user) => user.id === parseInt(req.params.id));
+  if (user) {
+    res.send(user);
+    return;
+  }
+  res.send({
+    message: "Error: User not found.",
+  });
 });
 
 export default router;
