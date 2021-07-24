425fe4d52722ad3f4a6b48799b4b12509891f651
eevan7a9 <e7a9@protonmail.com>
2021-07-24 16:49:24 +0900

    perf(users): updated then/catch & models

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -3,99 +3,129 @@ import { User } from "../models/users.js";
 export const getUsers = function (req, res) {
   console.log("Getting all users...");
   // find all users
-  User.find({}, (err, users) => {
-    if (err) {
-      console.log(err);
-      res.send({ message: `${err.reason}` });
-      return;
-    }
-    res.send({
-      message: "Success: Fetched all users.",
-      users,
+  User.find({})
+    .then((result) => {
+      res.send({
+        message: "Success: Fetched all users.",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send(err);
+        return;
+      }
     });
-  });
 };
 
 export const createUser = function (req, res) {
   console.log("Adding new user...");
 
   const { firstName, lastName, age } = req.body;
-  const user = new User({ firstName, lastName, age });
-  const message = "Success: New user added.";
+  const newUser = new User({ firstName, lastName, age });
   // saving to database
-  user.save((err, doc) => {
-    if (err) {
-      console.log(err);
-      res.send({ message: `${err.reason}` });
-      return;
-    }
-    res.send({
-      message,
-      data: doc,
+  newUser
+    .save()
+    .then((result) => {
+      res.status(201).send({
+        message: "Success: New user added.",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send(err);
+        return;
+      }
     });
-  });
 };
 
 export const getUser = function (req, res) {
   console.log("Getting single user...");
   const _id = req.params.id;
   // Finding by ID
-  User.findById({ _id }, (err, user) => {
-    if (err) {
-      console.log(err);
-      res.send({ message: `${err.reason}` });
-      return;
-    }
-    res.send({
-      message: "Success: Fetch single user.",
-      data: user,
+  User.findById({ _id })
+    .then((result) => {
+      if (!result) {
+        res.status(404).send({
+          message: "Error: User not found.",
+        });
+        return;
+      }
+      res.send({
+        message: "Success: Fetch single user.",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send(err);
+        return;
+      }
     });
-  });
 };
 
 export const updateUser = function (req, res) {
   console.log("Updating new user...");
   const _id = req.params.id;
   const { firstName, lastName, age } = req.body;
-
+  const $set = {
+    // Fields to update
+    ...(firstName !== undefined && { firstName }),
+    ...(lastName !== undefined && { lastName }),
+    ...(age !== undefined && { age }),
+  };
   User.findOneAndUpdate(
     { _id }, // ID
-    {
-      $set: {
-        // Fields to update
-        ...(firstName !== undefined && { firstName }),
-        ...(lastName !== undefined && { lastName }),
-        ...(age !== undefined && { age }),
-      },
-    },
-    { useFindAndModify: false, new: true }, // options
-    (err, user) => {
-      if (err) {
-        console.log(err);
-        res.send({ message: err.reason });
+    { $set },
+    { useFindAndModify: false, new: true } // options
+  )
+    .then((result) => {
+      if (!result) {
+        res.status(404).send({
+          message: "Error: User not found.",
+        });
         return;
       }
       res.send({
         message: "Success: Updated single user",
-        data: user,
+        data: result,
       });
-    }
-  );
+    })
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+        res.status(422).send(err);
+        return;
+      }
+    });
 };
 
 export const deleteUser = function (req, res) {
   console.log("Deleting user...");
   const _id = req.params.id;
 
-  User.findOneAndDelete({ _id }, (err, user) => {
-    if (err) {
-      console.log(err);
-      res.send({ message: err.reason });
-      return;
-    }
-    res.send({
-      message: "Success: User is deleted.",
-      data: user,
+  User.findOneAndDelete({ _id })
+    .then((result) => {
+      if (!result) {
+        res.status(404).send({
+          message: "Error: User not found.",
+        });
+        return;
+      }
+      res.send({
+        message: "Success: User is deleted.",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+        res.status(422).send({ message: err.reason });
+        return;
+      }
     });
-  });
 };
--- src/models/users.js
+++ src/models/users.js
@@ -1,9 +1,17 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  age: Number,
-});
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: String,
+    age: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const User = mongoose.model("User", userSchema);
